5b3af38ec6e5085067b0f0e8bb951ebe28fb8248
Aya Bouri <aya.bouri1@gmail.com>
2024-04-16 15:35:39 +0100

    update

---
+++ src/assets/css/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chafafproject.ClientApp.src.assets.css
+{
+    public class Class
+    {
+    }
+}
